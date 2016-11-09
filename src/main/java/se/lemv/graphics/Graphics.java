4f953009062065bebb3a009de24f8f084ca0790c
leo <leo.moralesvega@gmail.com>
2016-11-09 02:57:44 +0100

    Replaced old VBoxes with Group
    This allows images to stack on top of each other

--- src/main/java/se/lemv/graphics/Graphics.java
+++ src/main/java/se/lemv/graphics/Graphics.java
@@ -2,11 +2,15 @@ package se.lemv.graphics;
 
 import java.io.File;
 
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.effect.BlendMode;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -27,36 +31,39 @@ public class Graphics {
 		rootNode.getChildren().addAll(gridPane);
 		mainStage.setScene(scene);
 		mainStage.show();
+		testPawnImage(gridPane);
 	}
-	
+
+	private void testPawnImage(GridPane gridPane) {
+		Image pawn = new Image(loadImage("test_pawn.png").toURI().toString());
+		Group group = (Group) gridPane.getChildren().get(0);
+		ImageView imageView = (ImageView) group.getChildren().get(1);
+		imageView.setImage(pawn);
+	}
 	private GridPane createGridPane() {
-		VBox[][] imageView = createVBoxWithImageViewArray();
+		Group[][] groups = createGroupsWithImageView();
 		GridPane gridPane = new GridPane();
 		for(int i = 0; i < 8; i++){
 			for(int j = 0; j < 8; j++){
-				gridPane.add(imageView[i][j], i, j);
+				gridPane.add(groups[i][j], i, j);
 			}
 		}
 		return gridPane;
 	}
-	private VBox[][] createVBoxWithImageViewArray() {
-		VBox[][] vbox = new VBox[8][8];
+	private Group[][] createGroupsWithImageView() {
+		Group[][] group = new Group[8][8];
 		Image darkTile = getDarkTile();
 		Image lightTile = getLightTile();
 		
 		for(int i = 0; i < 8; i++){
 			for(int j = 0; j < 8; j++){
-				vbox[i][j] = makeStackPaneVBox();
 				if(i%2==0 && j%2==0 || i%2==1 && j%2==1)
-					vbox[i][j].getChildren().add(new ImageView(darkTile));
+					group[i][j] = new Group(new ImageView(darkTile), new ImageView());
 				else
-					vbox[i][j].getChildren().add(new ImageView(lightTile));
+					group[i][j] = new Group(new ImageView(lightTile), new ImageView());
 			}
 		}
-		return vbox;
-	}
-	private VBox makeStackPaneVBox() {
-		return new VBox(new StackPane());
+		return group;
 	}
 	private Image getDarkTile() {
 		return new Image(loadImage("dark_tile.png").toURI().toString());
